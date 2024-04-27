0af07a0355d3ca871aceaa0adf99e3616ccf872c
kneecapFolder <ilan7gr@gmail.com>
2024-04-28 00:27:37 +0300

    switched to a spritesheet based approach to tile rendering

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <map>
 #define CELL_SIZE 50
-#define BOARD_SIZE 10
+#define BOARD_WIDTH 10
+#define BOARD_HEIGHT 11
 
 using std::string;
 using std::to_string;
@@ -10,29 +11,39 @@ using std::map;
 
 struct Tile {
     Vector2 pos;
-    Texture2D sprite;
+    Vector2 source;
 };
 
-map<string, Texture2D> sprites;
+Texture2D spriteSheet;
+map<string, Vector2> sprites;
 
 void drawTile(Tile, Color);
 string vecToStr(Vector2);
 float mapf(float, float, float, float, float);
 
 int main() {
-    InitWindow(CELL_SIZE*BOARD_SIZE, CELL_SIZE*BOARD_SIZE, "Farming Game");
+    InitWindow(CELL_SIZE*BOARD_WIDTH, CELL_SIZE*BOARD_HEIGHT, "Farming Game");
     SetTargetFPS(60);
 
     // Load textures
-    sprites["dirt"] = LoadTexture("resources/dirtpxl.png");
+    spriteSheet = LoadTexture("resources/spritesheet.png");
+    sprites["dirt"] = (Vector2){0, 1};
+
+    map<string, Tile> background;
+
+    // Create Inventory
+    for(float i = 0; i < BOARD_WIDTH; i++)
+        background[vecToStr((Vector2){i, 0})] = (Tile){
+            (Vector2){i, 0},
+            (Vector2){i, 0},
+        };
 
     // Create Ground
-    map<string, Tile> ground;
-    for(float y = 0; y < BOARD_SIZE; y++)
-        for(float x = 0; x < BOARD_SIZE; x++)
-            ground[vecToStr((Vector2){x, y})] = (Tile){
+    for(float y = 1; y < BOARD_HEIGHT; y++)
+        for(float x = 0; x < BOARD_HEIGHT; x++)
+            background[vecToStr((Vector2){x, y})] = (Tile){
                 (Vector2){x, y},
-                sprites["dirt"]
+                sprites["dirt"],
             };
     
 
@@ -40,15 +51,15 @@ int main() {
     {
         // Input
         Vector2 mousePos = (Vector2){
-            mapf(GetMousePosition().x, 0, CELL_SIZE*BOARD_SIZE, 0, BOARD_SIZE),
-            mapf(GetMousePosition().y, 0, CELL_SIZE*BOARD_SIZE, 0, BOARD_SIZE),
+            mapf(GetMousePosition().x, 0, CELL_SIZE*BOARD_WIDTH, 0, BOARD_WIDTH),
+            mapf(GetMousePosition().y, 0, CELL_SIZE*BOARD_HEIGHT, 0, BOARD_HEIGHT),
         };
 
         // Draw
         BeginDrawing();
 
         ClearBackground(YELLOW);
-        for(auto const& [key, val] : ground)
+        for(auto const& [key, val] : background)
             drawTile(val, key == vecToStr(mousePos)? RED : WHITE);
 
         DrawText(vecToStr(mousePos).c_str(), 0, 0, 20, RED);
@@ -57,22 +68,27 @@ int main() {
     }
     
     // Unload textures from GPU
-    for(auto const& pair : sprites)
-        UnloadTexture(pair.second);
+    UnloadTexture(spriteSheet);
 
     CloseWindow();
     return 0;
 }
 
 void drawTile(Tile tile, Color tint) {
-    Rectangle source = (Rectangle){0, 0, tile.sprite.width, tile.sprite.height};
+    // 25 - tile size in the spritesheet
+    Rectangle source = (Rectangle){
+        tile.source.x * 25,
+        tile.source.y * 25,
+        25,
+        25
+    };
     Rectangle dest = (Rectangle){
         tile.pos.x * CELL_SIZE,
         tile.pos.y * CELL_SIZE,
         CELL_SIZE,
         CELL_SIZE
     };
-    DrawTexturePro(tile.sprite, source, dest, (Vector2){0, 0}, 0, tint);
+    DrawTexturePro(spriteSheet, source, dest, (Vector2){0, 0}, 0, tint);
 }
 
 string vecToStr(Vector2 vec) {
